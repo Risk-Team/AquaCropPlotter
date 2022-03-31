1ef97663526b2b312059f3a1b02e5e6ade58e8a7
TeeNattapongS <tee_smiler@hotmail.com>
2022-03-31 23:56:54 +0100

    update app to check correct file upload

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -15,13 +15,15 @@ ui <- dashboardPage(
         fileInput("upload_prm_files", "Upload parameter files (.PRM)", multiple = TRUE, accept = ".PRM"),
         
         #download all combined data
-        downloadButton("download", "Download combined data")
+        downloadButton("download_combined_dataset", "Download combined dataset")
     ),
     
     dashboardBody(
         dataTableOutput("upload_data_combined_display"),
         dataTableOutput("upload_prm_combined_display"),
-        dataTableOutput("data_prm_combined_display")
+        dataTableOutput("data_prm_combined_display"),
+        dataTableOutput("missing_prm_file_error"),
+        dataTableOutput("missing_prm_file_display")
         
     )
 )
@@ -32,6 +34,13 @@ server <- function(input, output, session) {
     ###read upload data files andcombine all data into dataframe
     upload_data_combined <-
         reactive({
+            #require uploaded data files before evaluating
+            req(input$upload_data_files)
+            #check to make sure uploaded files has the correct extension .OUT, return error if not 
+            upload_data_files_ext <- tools::file_ext(input$upload_data_files$name)
+            if(any(upload_data_files_ext != "OUT")){
+                validate("Invalid file: Please upload only .OUT files")
+            }
             #get a list of file paths from uploaded files
             input$upload_data_files %>%
                 #import dataset and clean up, format into dataframe
@@ -60,6 +69,13 @@ server <- function(input, output, session) {
     ###read uploaded parameter files and combine
     upload_prm_combined <-
         reactive({
+            #require uploaded prm files before evaluating
+            req(input$upload_prm_files)
+            #check to make sure uploaded files has the correct extension .prm, return error if not 
+            upload_prm_files_ext <- tools::file_ext(input$upload_prm_files$name)
+            if(any(upload_prm_files_ext != "PRM")){
+                validate("Invalid file: Please upload only .PRM files")
+            }
             #get a list of prm file path from uploaded
             input$upload_prm_files %>%
                 #read in each prm file from the list and extract parameter of interest
@@ -100,17 +116,37 @@ server <- function(input, output, session) {
     #output datatable of the combined parameters
     output$upload_prm_combined_display <- renderDataTable(upload_prm_combined())
     
+    ###check if all parameter .PRM files are uploaded as needed for all .OUT datasets
+    #find a list of any missing prm file required in the data files
+    missing_prm_file <- reactive({
+        upload_data_combined() %>%
+            select(prm.file) %>%
+            distinct() %>%
+            anti_join(upload_prm_combined(), by = c("prm.file" = "name"))
+    })
+    #return error if there is any missing prm files
+    output$missing_prm_file_error <- reactive({
+        if(nrow(missing_prm_file()) > 0){
+            validate("Missing .PRM files: Please upload the following .PRM files")
+        }
+    })
+    #display missing prm files
+    output$missing_prm_file_display <- renderDataTable(
+        if(nrow(missing_prm_file()) > 0){
+            missing_prm_file()
+        }
+    )
+
     ####add parameters to the output dataset
-    data_prm_combined <-
-        reactive({
+    data_prm_combined <- reactive({
             upload_data_combined() %>%
                 left_join(upload_prm_combined(), by = c("prm.file" = "name"))
         })
     #output datatable of the combined data and parameters
     output$data_prm_combined_display <- renderDataTable(data_prm_combined())
     #for downloading combined dataset
-    output$download <- downloadHandler(
-        filename = "combined_data.tsv",
+    output$download_combined_dataset <- downloadHandler(
+        filename = "Aquacrop_combined_data.tsv",
         content = function(file) {
             write_tsv(data_prm_combined(), file)
         }
