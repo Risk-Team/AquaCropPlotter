550313eaeb7cd5fcb61d60f5e7eb2720c4559714
TeeNattapongS <tee_smiler@hotmail.com>
2023-06-23 20:59:52 +0100

    boxplot code

---
+++ shinyapp/boxplot/boxplot.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+
+#modify currect plotting to boxplot
+#y axis select variable with values to plot
+#x axis now select grouping variable
+#allow color and facet grouping
+#select plot absolute vs relative change values
+#add line of reference baseline of average historical value
+#select reference year range
+#select split by time horizon
+
+data = read.delim("combined_seasonal_data.tsv")
+
+prm = read.delim("combined_parameter_data (1).tsv")
+
+ggplot(data, aes(y=BioMass, x=name.variable3, fill=name.variable7))+
+  stat_boxplot(geom="errorbar")+#add horizontal line to end of whiskers
+  geom_boxplot()+
+  #geom_point(position = position_jitterdodge(jitter.width = 0.2), size=0.1)
+  facet_grid(.~name.variable4)
+
+#select reference year range
+ref.year = c(2020,2030)
+xaxis.var = "name.variable3"
+grouping.var = "name.variable7"
+
+data.ref = data %>%
+  filter(Year1 %in% c(ref.year[1]:ref.year[2])) %>%
+  select(!starts_with(c("Day","Month","Year"))) %>%
+  group_by(across(all_of(c(xaxis.var, grouping.var)))) %>%
+  summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE))) 
+
+data.norm = data %>%
+  group_by(across(all_of(c(xaxis.var, grouping.var)))) %>%
+  mutate(across(where(is.numeric), ~ 100*(.x - mean(.x[which(Year1 %in% c(ref.year[1]:ref.year[2]))], na.rm = TRUE))/mean(.x[which(Year1 %in% c(ref.year[1]:ref.year[2]))], na.rm = TRUE) )) %>%
+  select(!starts_with(c("Day","Month","Year")))
+  
+ggplot(data.norm, aes(y=BioMass, x=name.variable3, fill=name.variable7))+
+  stat_boxplot(geom="errorbar")+#add horizontal line to end of whiskers
+  geom_boxplot()+
+  #geom_point(position = position_jitterdodge(jitter.width = 0.2), size=0.1)
+  facet_grid(.~name.variable4)
+
+
+  
